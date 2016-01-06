42c6e37058197a6158034a0ba3b6807067e7fb41
Hernan Yamakawa <hernan.yamakawa@gmail.com>
2016-01-06 11:50:24 -0300

    Updated to Grails 2.4.4, fixed REST JS error

--- grails-app/assets/javascript/angular/cool-demo.js
+++ grails-app/assets/javascript/angular/cool-demo.js
@@ -51,6 +51,6 @@ window.ngCoolDemo.ng = angular.module('ngCoolDemo', [
         });
 
         $httpProvider.interceptors.push('DefaultHttpInterceptor');
-        $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+        //$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     }]
 );
